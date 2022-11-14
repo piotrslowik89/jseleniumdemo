f1d896162d12013acf73fde2ee0789f69883b0df
Piotr Slowik <piotrslowik409@gmail.com>
2022-11-14 15:39:13 +0100

    add tests and page objects|

--- src/test/java/pl/nahtingale/tests/CheckoutTest.java
+++ src/test/java/pl/nahtingale/tests/CheckoutTest.java
@@ -13,13 +13,16 @@ public class CheckoutTest extends BaseTest {
         Customer customer = new Customer();
         customer.setEmail("specificemail@gmail.com");
 
-        OrderDetailsPage orderDetailsPage =
-                new HomePage(driver).openShopPage().openProduct("Java Selenium WebDriver")
-                        .addProductToCart().viewCart().openAddressDetails()
-                        .fillAddressDetails(customer, "Some random comment");
+        OrderDetailsPage orderDetailsPage = new HomePage(driver)
+                .openShopPage()
+                .openProduct("Java Selenium WebDriver")
+                .addProductToCart()
+                .viewCart()
+                .openAddressDetails()
+                .fillAddressDetails(customer, "Some random comment");
 
         Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(), "Thank you. Your order has been received.");
-        Assert.assertEquals(orderDetailsPage.getProductName().getText(),  "Java Selenium WebDriver × 1");
+        Assert.assertEquals(orderDetailsPage.getProductName().getText(), "Java Selenium WebDriver × 1");
     }
 
 }
