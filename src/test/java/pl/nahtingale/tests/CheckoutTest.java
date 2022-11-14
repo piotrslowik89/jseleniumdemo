610c9ee558bc062183f357dbccfd453a6d248840
Piotr Slowik <piotrslowik409@gmail.com>
2022-11-14 15:26:56 +0100

    adding assertions

--- src/test/java/pl/nahtingale/tests/CheckoutTest.java
+++ src/test/java/pl/nahtingale/tests/CheckoutTest.java
@@ -1,17 +1,25 @@
 package pl.nahtingale.tests;
 
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pl.nahtingale.models.Customer;
 import pl.nahtingale.pages.HomePage;
+import pl.nahtingale.pages.OrderDetailsPage;
 
 public class CheckoutTest extends BaseTest {
 
     @Test
     public void checkoutTest() {
+        Customer customer = new Customer();
+        customer.setEmail("specificemail@gmail.com");
 
-        new HomePage(driver).openShopPage().openProduct( "Java Selenium WebDriver")
-                .addProductToCart().viewCart().openAddressDetails();
+        OrderDetailsPage orderDetailsPage =
+                new HomePage(driver).openShopPage().openProduct("Java Selenium WebDriver")
+                        .addProductToCart().viewCart().openAddressDetails()
+                        .fillAddressDetails(customer, "Some random comment");
+
+        Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(), "Thank you. Your order has been received.");
+        Assert.assertEquals(orderDetailsPage.getProductName().getText(),  "Java Selenium WebDriver × 1");
     }
 
 }
